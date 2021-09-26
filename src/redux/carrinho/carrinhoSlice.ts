ebfbc754f179c94042a993830d2892f05a6fa91c
altieresl <altieresluciano@deltainova.com.br>
2021-09-26 00:34:04 -0300

    feat: alterando regra da função de adicionar ao carrinho

--- src/redux/carrinho/carrinhoSlice.ts
+++ src/redux/carrinho/carrinhoSlice.ts
@@ -9,7 +9,24 @@ export const carrinhoSlice = createSlice({
   initialState,
   reducers: {
     addItemCarrinho: (state, { payload }) => {
-      state.itensCarrinho.push(payload);
+      let posicaoProdutoAdicionado = state.itensCarrinho.findIndex(
+        (element: any) => {
+          return element.id == payload.id;
+        }
+      );
+      // Se o produto ainda não está no carrinho, adiciona ao array
+      if (posicaoProdutoAdicionado === -1) {
+        state.itensCarrinho.push({ ...payload, quantity: 1 });
+      } else {
+        if (
+          payload.stock > state.itensCarrinho[posicaoProdutoAdicionado].quantity
+        ) {
+          // Se o produto já estiver no carrinho e existir estoque, apenas aumenta a quantidade
+          state.itensCarrinho[posicaoProdutoAdicionado].quantity += 1;
+        } else {
+          throw 'Ops, o estoque desse produto esgotou :(';
+        }
+      }
     },
     removeItemCarrinho: (state, { payload }) => {
       const { id } = payload;
@@ -17,11 +34,15 @@ export const carrinhoSlice = createSlice({
       state.itensCarrinho = state.itensCarrinho.filter(
         (item: { id: any }) => item.id !== id
       );
+    },
+    limparItensCarrinho: (state) => {
+      state.itensCarrinho = [];
     }
   }
 });
 
-export const { addItemCarrinho, removeItemCarrinho } = carrinhoSlice.actions;
+export const { addItemCarrinho, removeItemCarrinho, limparItensCarrinho } =
+  carrinhoSlice.actions;
 export const getItensCarrinho = (state: {
   itensCarrinho: { itensCarrinho: any };
 }) => state.itensCarrinho.itensCarrinho;
